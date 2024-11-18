9be338a8213e648782011a2f99bdfde8cfc40ab3
epicgk11 <epicgeek11@gmail.com>
2024-11-18 20:24:14 +0530

    added update task functionality completely

--- efficientlyApp/views.py
+++ efficientlyApp/views.py
@@ -3,9 +3,17 @@ from django.http import JsonResponse
 from .utils import parse
 import requests
 
+
+base_url = f""
+def get_base_path(request):
+    global base_url
+    if base_url == "":
+        base_url = f"http://{request.get_host()}"
+        return
+    
 def createtask(request):
     if request.method == 'POST':
-        base_url = f"http://{request.get_host()}"
+        get_base_path(request)
         json_data = parse(request)
         res = requests.post(f"{base_url}/data/create/",json = json_data)
         if res.status_code == 200:
@@ -15,13 +23,13 @@ def createtask(request):
     return render(request, 'createTask.html')
 
 def listtasks(request):
-    base_url = f"http://{request.get_host()}"
+    get_base_path(request)
     tasks = dict(requests.get(f"{base_url}/data/list").json())
     context = {"tasks":tasks['tasks']}
     return render(request,'home.html',context)
 
 def deletetask(request,taskId):
-    base_url = f"http://{request.get_host()}"
+    get_base_path(request)
     res = requests.delete(f"{base_url}/data/delete/{taskId}/")
     if res.status_code == 200:
         return redirect('listtasks')
@@ -30,8 +38,10 @@ def deletetask(request,taskId):
     
 def updatetask(request,taskId):
     if request.method == "POST":
-        print(request.POST.getlist('step_completed[]'))
-    base_url = f"http://{request.get_host()}"
+        get_base_path(request)
+        dictData = parse(request)
+        requests.post(f"{base_url}/data/update/{taskId}/",json = dictData)
+        return redirect('listtasks')
     res = requests.get(f"{base_url}/data/get/{taskId}")
     context = {'task':res.json()['task']}
     return render(request,'taskView.html',context=context)
